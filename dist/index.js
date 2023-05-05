3fb7a631138ad6ba14bd68dac9e4bdc4bdd36b2d
Ernane Ferreira <ernane.junior25@gmail.com>
2023-05-04 21:55:37 -0300

    fix: build

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,121 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ 5105:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+/* module decorator */ module = __nccwpck_require__.nmd(module);
+const STATUS_OPTIONS = {
+  success: {
+    status: 'Successful action run',
+    color: 0x28A745
+  },
+  failure: {
+    status: 'Failed action run',
+    color: 0xCB2431
+  },
+  cancelled: {
+    status: 'Canceled action run',
+    color: 0xDBAB09
+  }
+}
+
+module.export = { STATUS_OPTIONS }
+
+
+/***/ }),
+
+/***/ 918:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+/* module decorator */ module = __nccwpck_require__.nmd(module);
+const { STATUS_OPTIONS } = __nccwpck_require__(5105);
+
+const core = __nccwpck_require__(2186);
+const github = __nccwpck_require__(5438);
+const axios = __nccwpck_require__(8757);
+
+function getInputs() {
+  return {
+    webhook:      core.getInput('webhook', {required: false}),
+    status:       core.getInput('status', {required: false}).toLowerCase(),
+    content:      core.getInput('content', {required: false}),
+    title:        core.getInput('custom_title', {required: false}),
+    description:  core.getInput('description', {required: false}),
+    image:        core.getInput('image', {required: false}),
+    color:        core.getInput('color', {required: false}),
+    username:     core.getInput('username', {required: false}),
+    avatar_url:   core.getInput('avatar_url', {required: false}),
+    event_info:   core.getInput('event_info', {required: false}),
+    timestamp:    core.getInput('timestamp', {required: false})
+  }
+}
+
+function getDiscordPayload(inputs) {
+  const githubContext = github.context
+  const { owner, repo } = githubContext.repo
+  const { ref, workflow, actor, payload, serverUrl, runId } = githubContext
+  const repoURL = `${serverUrl}/${owner}/${repo}`
+  const workflowURL = `${repoURL}/actions/runs/${runId}`
+
+  const eventDetail = `[${payload.head_commit.author.name}](https://github.com/${actor}) authored & committed - ${payload.head_commit.message}`
+  const [ refs, head, branch ] = ref.split('/')
+
+  let embed = {
+    color: inputs.color || STATUS_OPTIONS[inputs.status].color,
+    footer: { text: `Triggered by ${actor}`, icon_url: `https://github.com/${actor}.png?size=32` },
+    thumbnail: { url: 'https://github.com/github.png' }
+  }
+
+  if (inputs.title) {
+    embed.title = inputs.title
+  }else{
+    embed.title = `${STATUS_OPTIONS[inputs.status].status}`
+  }
+
+  if (inputs.description) {
+    embed.description = inputs.description
+  }else{
+    embed.description = eventDetail
+  }
+
+  if (inputs.timestamp) embed.timestamp = (new Date()).toISOString()
+  if (inputs.image) embed.image = { url: inputs.image }
+
+  if (inputs.event_info) {
+    embed.fields = [
+      { name: '', value: ``, inline: false },
+      { name: 'Repository', value: `[${owner}/${repo}](${repoURL})`, inline: true },
+      { name: 'Branch', value: `[${branch}](${repoURL}/tree/${branch})`, inline: true },
+      { name: '', value: ``, inline: false },
+      { name: 'Commit', value: `[\`${payload.head_commit.id.substring(0, 7)}\`](${payload.head_commit.url})`, inline: true },
+      { name: 'Workflow', value: `[${workflow}#${runId}](${workflowURL})`, inline: true },
+      { name: '', value: ``, inline: false },
+    ]
+  }
+
+  // embeds: [fitEmbed(embed)]
+  let discord_payload = { embeds: [embed] }
+
+  if (inputs.username) discord_payload.username = inputs.username
+  if (inputs.avatar_url) discord_payload.avatar_url = inputs.avatar_url
+  if (inputs.content) discord_payload.content = inputs.content
+
+  return discord_payload
+}
+
+async function sendPayload(webhook, payload) {
+  try {
+    await axios.post(webhook, payload)
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+module.export = { getInputs, getDiscordPayload, sendPayload }
+
+/***/ }),
+
 /***/ 7351:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -16299,8 +16414,8 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
+/******/ 			id: moduleId,
+/******/ 			loaded: false,
 /******/ 			exports: {}
 /******/ 		};
 /******/ 	
@@ -16313,11 +16428,23 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 /******/ 			if(threw) delete __webpack_module_cache__[moduleId];
 /******/ 		}
 /******/ 	
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/ 	
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/node module decorator */
+/******/ 	(() => {
+/******/ 		__nccwpck_require__.nmd = (module) => {
+/******/ 			module.paths = [];
+/******/ 			if (!module.children) module.children = [];
+/******/ 			return module;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
@@ -16326,137 +16453,13 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
-const core = __nccwpck_require__(2186);
-const github = __nccwpck_require__(5438);
-const axios = __nccwpck_require__(8757);
-
-const STATUS_OPTIONS = {
-  success: {
-    status: 'Successful action run',
-    color: 0x28A745
-  },
-  failure: {
-    status: 'Failed action run',
-    color: 0xCB2431
-  },
-  cancelled: {
-    status: 'Canceled action run',
-    color: 0xDBAB09
-  }
-}
-
-function getInputs() {
-  return {
-    webhook:      core.getInput('webhook', {required: false}),
-    status:       core.getInput('status', {required: false}).toLowerCase(),
-    content:      core.getInput('content', {required: false}),
-    title:        core.getInput('title', {required: false}),
-    description:  core.getInput('description', {required: false}),
-    image:        core.getInput('image', {required: false}),
-    color:        core.getInput('color', {required: false}),
-    url:          core.getInput('url', {required: false}),
-    username:     core.getInput('username', {required: false}),
-    avatar_url:   core.getInput('avatar_url', {required: false}),
-    event_info:   core.getInput('event_info', {required: false}),
-    full_title:   core.getInput('full_title', {required: false}),
-    timestamp:  core.getInput('timestamp', {required: false})
-  }
-}
-
-function getDiscordPayload(inputs) {
-  const ctx = github.context
-  const { owner, repo } = ctx.repo
-  const { ref, workflow, actor, payload, serverUrl, runId } = ctx
-  const repoURL = `${serverUrl}/${owner}/${repo}`
-  const workflowURL = `${repoURL}/actions/runs/${runId}`
-
-  const eventDetail = `[${payload.head_commit.author.name}](https://github.com/${actor}) authored & committed - ${payload.head_commit.message}`
-  const [ refs, head, branch ] = ref.split('/')
-
-  let embed = {
-    color: inputs.color || STATUS_OPTIONS[inputs.status].color,
-    footer: { text: `Triggered by ${actor}`, icon_url: `https://github.com/${actor}.png?size=32` },
-    thumbnail: { url: 'https://github.com/github.png' }
-  }
-
-  if (inputs.timestamp) {
-    embed.timestamp = (new Date()).toISOString()
-  }
-
-  embed.title = `${STATUS_OPTIONS[inputs.status].status}`
-  
-  if (inputs.image) {
-    embed.image = { url: inputs.image }
-  }
-
-  if (inputs.description) {
-    embed.description = inputs.description
-  }else{
-    embed.description = eventDetail
-  }
-
-  if (inputs.event_info) {
-    embed.fields = [
-      { name: '', value: ``, inline: false },
-      {
-        name: 'Repository',
-        value: `[${owner}/${repo}](${repoURL})`,
-        inline: true
-      },
-      {
-        name: 'Branch',
-        value: `[${branch}](${repoURL}/tree/${branch})`,
-        inline: true
-      },
-      { name: '', value: ``, inline: false },
-      {
-        name: 'Commit',
-        value: `[\`${payload.head_commit.id.substring(0, 7)}\`](${payload.head_commit.url})`,
-        inline: true
-      },
-      {
-        name: 'Workflow',
-        value: `[${workflow}#${runId}](${workflowURL})`,
-        inline: true
-      },
-      { name: '', value: ``, inline: false },
-    ]
-  }
-
-  let discord_payload = {
-    // embeds: [fitEmbed(embed)]
-    embeds: [embed]
-  }
-
-  if (inputs.username) {
-    discord_payload.username = inputs.username
-  }
-  if (inputs.avatar_url) {
-    discord_payload.avatar_url = inputs.avatar_url
-  }
-  if (inputs.content) {
-    discord_payload.content = inputs.content
-  }
-
-  return discord_payload
-}
-
-async function sendMessage(webhook, payload) {
-  try {
-    await axios.post(webhook, payload)
-  } catch (error) {
-    core.setFailed(error.message);
-  }
-}
+const { getInputs, getDiscordPayload, sendPayload } = __nccwpck_require__(918);
 
 const main = async () => {
   const inputs = getInputs();
-  core.info(JSON.stringify(inputs))
-  core.info(JSON.stringify(STATUS_OPTIONS))
   const payload = getDiscordPayload(inputs);
-  core.info(JSON.stringify(payload))
 
-  await sendMessage(inputs.webhook.trim(), payload)
+  await sendPayload(inputs.webhook.trim(), payload)
 }
 
 main();
